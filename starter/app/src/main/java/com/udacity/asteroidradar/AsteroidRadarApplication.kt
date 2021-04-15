fb52d7b2d1809bcf1a1efcec6ad01f6788fc233c
Peter Stackebrandt <peter.stackebrandt@immowelt.de>
2021-04-15 17:40:25 +0200

    update gradle files - put version numbers into project gradle - sort numbers https://github.com/pstackebrandt/asteroid-radar/projects/1#card-59102295

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -0,0 +1,17 @@
+package com.udacity.asteroidradar
+
+import android.app.Application
+import timber.log.Timber
+
+class AsteroidRadarApplication : Application() {
+    /**
+     * onCreate is called before the first screen is shown to the user.
+     *
+     * Use it to setup any background tasks, running expensive setup operations in a background
+     * thread to avoid delaying app start.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
